445d30ded78bb9a779ad2165d9ba18661149f4ab
Jorge Rodriguez <jokercg1@gmail.com>
2025-07-20 12:37:38 -0400

    feat: agregar modal con formulario de contacto y actualizar index.html

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,25 @@
 //JavaScript
+document.addEventListener("DOMContentLoaded", function () {
+document.getElementById('telefono').addEventListener('input', function (e) {
+  let valor = e.target.value.replace(/\D/g, '');
+  
+
+  if (valor.length > 0) {
+    valor = valor.substring(0, 9); 
+    valor = valor.replace(/^(\d{1})(\d{4})(\d{4})$/, '$1-$2-$3');
+  }
+  
+  e.target.value = valor;
+});
+
+const form = document.querySelector("form")
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
+  document.getElementById("mensajeEnviado").style.display = "block"
+  this.reset()
+});
+
+const myModal = document.getElementById('myModal')
+const myInput = document.getElementById('myInput')
+
+})
